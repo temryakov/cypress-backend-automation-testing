d11c7f7d026a77ffcb294b8b6ae7090862c4b9ed
Maxim Temryakov <therevors@gmail.com>
2023-06-27 10:56:10 +0300

    Refactored spec files for actual classes

--- cypress/api/entity/entity.cy.js
+++ cypress/api/entity/entity.cy.js
@@ -4,25 +4,18 @@ const RequestManager = require("../../modules/RequestManager");
 const ResponseAssertion = require("../../modules/responseAssertion")
 const { entityBody } = require("./entity.js");
 
-let token; 
-let identity; 
-
 before(() => {
-  return AuthHelper.login().then(() => {
-    token = AuthHelper.getAccessToken(); 
-  })
+  return AuthHelper.login()
 })
 
 before(() => {
-  RequestManager.sendIdentityRequest(token).then((response) => {
-    identity = response 
-  })
+  return AuthHelper.getIdentity()
 })
 
 describe('entity', () => {
   it('Entity test case #1', () => {    
-    RequestManager.sendPostRequest('/api/entity/', entityBody, token).should((response) => {
-      let permission = PermissionsParser.assertScope(identity, 'entity', 'create')
+    RequestManager.sendPostRequest('/api/entity/', entityBody).should((response) => {
+      let permission = PermissionsParser.assertScope(cy.identity, 'entity', 'create')
       if (permission === null) {
         ResponseAssertion.errorForbidden(response)
       } else {
--- cypress/graphql/Profile/profilePage.cy.js
+++ cypress/graphql/Profile/profilePage.cy.js
@@ -3,19 +3,22 @@ const RequestManager = require("/cypress/modules/requestManager.js");
 const { achievementQuery } = require("./profilePage.js");
 const ResponseAssertion = require("../../modules/responseAssertion.js");
 
-let token; 
+before(() => {
+  return AuthHelper.login()
+})
 
 before(() => {
-  return AuthHelper.login().then(() => {
-    token = AuthHelper.getAccessToken(); 
-  })
+  return AuthHelper.getIdentity()
 })
 
 describe('profilePage', () => {
-  it('getProfile', () => {    
-    RequestManager.sendGraphQLRequest(achievementQuery, {"userId": 123}, token).should((response) => {
-      ResponseAssertion.successOk(response)
-    })
+  it('getProfile', () => {
+      RequestManager.sendGraphQLRequest(
+          achievementQuery,
+          {"userId": cy.identity.user.id}
+      )
+          .then((response) => {
+              ResponseAssertion.successOk(response)
+          })
   })
 })
-
