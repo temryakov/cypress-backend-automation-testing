a37f4d34caae1744585ddae068ff79d8beeb8b9d
Maxim Temryakov <therevors@gmail.com>
2023-06-29 11:06:33 +0300

    Implemented handling if user permissions aren't exist

--- cypress/modules/PermissionsParser.js
+++ cypress/modules/PermissionsParser.js
@@ -1,12 +1,18 @@
 class PermissionsParser {
     getArray(identity, permissionType) {
-        const arr = identity.permissions
+        const arr = identity.permissions;
         const permission = arr.find(item => item.rsname === permissionType);
-
-        return permission.scopes // ['read', 'add', etc]
+        
+        if (typeof permission !== 'undefined' && typeof permission.scopes !== 'undefined') {
+            return permission.scopes; // ['read', 'add', etc]
+        }
+        return null;
     }
     assertScope(identity, permissionType, scopeToCheck) {
         let scopesArray = this.getArray(identity, permissionType)
+
+        if (scopesArray === null) return null;
+
         for (const scope of scopesArray) {
             if (scope === scopeToCheck) {
                 return scope;
